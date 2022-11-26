ffc6e65abb2ee78589c51130dbf0e2f9f20bf16d
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-11-26 18:08:47 +0100

    Grid setup

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import Sketch from "react-p5";
-import p5Types from "p5"; //Import this for typechecking and intellisense
+import p5Types from "p5";
 
 interface GridProps {
   //Your component props
 }
 
-let x = 50;
-const y = 50;
+const [gridRows, gridCols] = [40, 20];
+const [canvasWidth, canvasHeight] = [800, 400];
+
+let [cellWidth, cellHeight] = [canvasWidth / gridRows, canvasHeight / gridCols];
 
 const Grid: React.FC<GridProps> = (props: GridProps) => {
-  //See annotations in JS for more information
   const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.frameRate(5);
+    p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
   };
 
   const draw = (p5: p5Types) => {
     p5.background(0);
-    p5.ellipse(x, y, 70, 70);
-    x++;
+    for (let i = 0; i < gridRows; i++) {
+      for (let j = 0; j < gridCols; j++) {
+        let alive = true;
+        if (Math.random() < 0.8) {
+          alive = false;
+        }
+        p5.fill(alive ? 255 : 80);
+        p5.stroke("black");
+        p5.strokeWeight(1);
+        p5.rect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+      }
+    }
   };
 
   return <Sketch setup={setup} draw={draw} />;
